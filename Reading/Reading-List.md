1a3109cc243fcb77350b91b3cadc3bde238f32ef
leo0807 <zhangjunxu3@gmail.com>
2022-01-17 20:13:46 +0800

    basic solidity syntax

--- Reading/Reading-List.md
+++ Reading/Reading-List.md
@@ -1,1 +1,3 @@
-[Learning Blockchain development #day1 - As a frontend developer.](https://dev.to/kritik/learning-blockchain-development-day1-as-a-frontend-developer-579b)
+- [Learning Blockchain development #day1 - As a frontend developer.](https://dev.to/kritik/learning-blockchain-development-day1-as-a-frontend-developer-579b)
+
+- [My first impressions of web3](https://moxie.org/2022/01/07/web3-first-impressions.html)
---
+++ Solidity/basic.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract SimpleStorage {
+    uint256 public favoriteNumber = 5;
+    bool favoriteBool = true;
+    string favoriteString = "string";
+    int256 favoriteInt = -5;
+
+    address favoriteAddr = 0xD27b710E4d0e6177a459b206bc7c45Fff980295f;
+    bytes32 favoriteBytes = "cat";
+
+    struct People {
+        uint256 favNum;
+        string name;
+    }
+
+    People[] public people;
+
+    function store(uint256 _favoriteNum) public {
+        favoriteNumber = _favoriteNum;
+    }
+
+    // view 可以显示contract的state
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favriteNum) public {
+        people.push(People(_favriteNum, _name));
+    }
+}
